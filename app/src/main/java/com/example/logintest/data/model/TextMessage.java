a5624fc17208f40c069e5cde025700658256c98b
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-05-09 11:23:56 -0700

    Basic Message Shown pragmatically done

---
+++ app/src/main/java/com/example/logintest/data/model/TextMessage.java
@@ -0,0 +1,22 @@
+package com.example.logintest.data.model;
+
+public class TextMessage {
+    int userId;
+    int text;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getText() {
+        return text;
+    }
+
+    public void setText(int text) {
+        this.text = text;
+    }
+}
---
+++ app/src/main/java/com/example/logintest/message/MessageActivity.java
@@ -0,0 +1,72 @@
+package com.example.logintest.message;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.logintest.data.model.LoggedInUser;
+import com.example.logintest.data.model.User;
+import com.example.logintest.databinding.ActivityMessageBinding;
+
+public class MessageActivity extends AppCompatActivity {
+
+    ActivityMessageBinding binding;
+    float scale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        scale = MessageActivity.this.getResources().getDisplayMetrics().density;
+        super.onCreate(savedInstanceState);
+        binding = ActivityMessageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        User user1 = new User("1", "Ivan", getResources().getIdentifier("ic_user1",
+                "drawable", getPackageName()));
+
+        User user2 = new User("2", "Ivannia", getResources().getIdentifier("ic_user2",
+                "drawable", getPackageName()));
+
+        LinearLayout newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
+                "long text message that should write at leat 2 sentences");
+
+        binding.linearLayoutFull.addView(newMessage1);
+
+        LinearLayout newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
+                "long text message that should write at leat 2 sentences");
+
+        binding.linearLayoutFull.addView(newMessage);
+    }
+
+    public LinearLayout createNewMessageDisplay(User user, String message){
+        LinearLayout box = new LinearLayout(MessageActivity.this);
+        box.setOrientation(LinearLayout.HORIZONTAL);
+        box.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+
+        ImageView userImage = new ImageView(MessageActivity.this);
+        userImage.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+        userImage.getLayoutParams().height = (int) (50 * scale + 0.5f);
+        userImage.getLayoutParams().width = (int) (50 * scale + 0.5f);
+        userImage.setImageResource(user.getUserIcon());
+        box.addView(userImage);
+
+        LinearLayout text = new LinearLayout(MessageActivity.this);
+        text.setOrientation(LinearLayout.VERTICAL);
+        text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        TextView userName = new TextView(MessageActivity.this);
+        userName.setText(user.getDisplayName());
+        text.addView(userName);
+        TextView textMessage = new TextView(MessageActivity.this);
+        textMessage.setText(message);
+        text.addView(textMessage);
+
+        box.addView(text);
+
+        return box;
+    }
+}
