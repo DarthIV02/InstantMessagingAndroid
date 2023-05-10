d8a74e475e8c4eb1ffa410cc01629b252cd5434f
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-05-10 13:16:36 -0700

    Initial Test of Database

--- app/src/main/java/com/example/logintest/message/MessageActivity.java
+++ app/src/main/java/com/example/logintest/message/MessageActivity.java
@@ -1,25 +1,52 @@
 package com.example.logintest.message;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.logintest.data.model.LoggedInUser;
 import com.example.logintest.data.model.User;
 import com.example.logintest.databinding.ActivityMessageBinding;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.Set;
 
 public class MessageActivity extends AppCompatActivity {
 
     ActivityMessageBinding binding;
     float scale;
+
+    // creating a variable for
+    // our Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our
+    // Database Reference for Firebase.
+    DatabaseReference databaseReference;
+
+    LinearLayout newMessage;
+
+    final public String TAG = "IVANNIA DEBUGGING";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,85 +55,19 @@ public class MessageActivity extends AppCompatActivity {
         binding = ActivityMessageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Log.v(TAG, FirebaseApp.getInstance().getOptions().getProjectId());
+
         // MESSAGES ADDED WITH FIREBASE START
 
-        User user1 = new User("1", "Ivan", getResources().getIdentifier("ic_user1",
-                "drawable", getPackageName()));
+        // below line is used to get the instance
+        // of our Firebase database.
+        firebaseDatabase = FirebaseDatabase.getInstance();
 
-        User user2 = new User("2", "Ivannia", getResources().getIdentifier("ic_user2",
-                "drawable", getPackageName()));
+        // below line is used to get
+        // reference for our database.
+        databaseReference = firebaseDatabase.getReference("messages");
 
-        LinearLayout newMessage1;
-        LinearLayout newMessage;
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage);
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage);
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage);
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage);
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage);
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is Ivannia's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage);
-
-        newMessage1 = createNewMessageDisplay(user1, "This is Ivan's my test message just like I was saying this is a really" +
-                "long text message that should write at leat 2 sentences");
-
-        binding.linearLayoutFull.addView(newMessage1);
-
-        newMessage = createNewMessageDisplay(user2, "This is the last message from me");
-
-        binding.linearLayoutFull.addView(newMessage);
+        getdata();
 
         // MESSAGES ADDED WITH FIREBASE END
 
@@ -137,7 +98,13 @@ public class MessageActivity extends AppCompatActivity {
         });
     }
 
-    public LinearLayout createNewMessageDisplay(User user, String message){
+    public LinearLayout createNewMessageDisplay(String user_name, String message){
+
+        // TO DO: Change to make it dynamic
+
+        User user = new User("1", user_name, getResources().getIdentifier("ic_user1",
+                "drawable", getPackageName()));
+
         LinearLayout box = new LinearLayout(MessageActivity.this);
         box.setOrientation(LinearLayout.HORIZONTAL);
         box.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
@@ -164,5 +131,49 @@ public class MessageActivity extends AppCompatActivity {
         box.addView(text);
 
         return box;
+    }
+
+    private void getdata() {
+
+        // calling add value event listener method
+        // for getting the values from database.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                JSONObject messages = (JSONObject) snapshot.getValue(JSONObject.class);
+
+                Iterator< ? > keys = messages.keys();
+
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    try {
+                        if (messages.get(key) instanceof JSONObject) {
+                            JSONObject message = (JSONObject) messages.get(key);
+                            String user_name = message.getString("name");
+                            String text = message.getString("text");
+
+                            newMessage = createNewMessageDisplay(user_name, text);
+
+                            binding.linearLayoutFull.addView(newMessage);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // calling on cancelled method when we receive
+                // any error or we are not able to get the data.
+                Toast.makeText(MessageActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
