f724d2fbbfa187bd99f84509f89631193a1cf5ce
Ivan Sanchez <ivanss@cetys.edu.mx>
2023-05-15 20:48:49 -0700

    added functionality

--- app/src/main/java/com/example/logintest/ui/login/LoadingActivity.java
+++ app/src/main/java/com/example/logintest/ui/login/LoadingActivity.java
@@ -46,6 +46,7 @@ public class LoadingActivity extends AppCompatActivity {
         {
             Intent intent = new Intent(LoadingActivity.this, LoginActivity.class);
             startActivity(intent);
+            finish();
         }
     }
 }
--- app/src/main/java/com/example/logintest/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/logintest/ui/login/RegisterActivity.java
@@ -3,13 +3,24 @@ package com.example.logintest.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.logintest.R;
-import com.example.logintest.databinding.ActivityLoadingBinding;
 import com.example.logintest.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
     ActivityRegisterBinding binding;
+    Button RegisterButton;
+    EditText userNameEditText;
+    EditText passwordEditText;
+    EditText confirmPasswordEditText;
+    EditText emailEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,54 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        RegisterButton = binding.buttonRegister;
+        userNameEditText = binding.editTextName;
+        passwordEditText = binding.editTextPassword;
+        confirmPasswordEditText = binding.editTextConfirmPassword;
+        emailEditText = binding.editTextEmail;
+
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = userNameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirmPassword = confirmPasswordEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                // Check if passwords match
+                if (userName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.equals(confirmPassword)) {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(task -> {
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                    Toast.makeText(RegisterActivity.this, "Username already taken", Toast.LENGTH_SHORT).show();
+                                }
+                                if (task.isSuccessful()) {
+                                    // User account created successfully
+                                    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                    DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+                                    usersRef.child(uid).child("email").setValue(email);
+                                    usersRef.child(uid).child("username").setValue(userName);
+                                    Toast.makeText(RegisterActivity.this, "Register succesfull", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "fallooooooo", Toast.LENGTH_SHORT).show();
+                                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                        Toast.makeText(RegisterActivity.this, "Username already taken", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
 
     }
 }
