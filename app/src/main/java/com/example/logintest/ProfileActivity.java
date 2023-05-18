7ba143708ac7a1f302be6f1420d0d207c5a761d4
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-05-18 12:15:52 -0700

    Download Image with Glide :)

--- app/src/main/java/com/example/logintest/ProfileActivity.java
+++ app/src/main/java/com/example/logintest/ProfileActivity.java
@@ -2,13 +2,42 @@ package com.example.logintest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.example.logintest.data.model.GlideApp;
+import com.example.logintest.databinding.ActivityProfileBinding;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.InputStream;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ActivityProfileBinding binding;
+    ImageView userImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        userImageView = binding.userImage;
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference conic = storageRef.child("1conic.jpg");
+
+        GlideApp.with(this /* context */)
+                .load(conic)
+                .into(userImageView);
+
     }
 }
